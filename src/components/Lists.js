f3572adb0368563632d0cd43c272c71ca1ae147d
Enebye <Michelle-Chantal@hotmail.com>
2022-10-28 12:26:03 +0200

    Michelle

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -12,13 +12,16 @@ export default function Lists() {
         <>
          <div className="List_one">
          <h4>Personal doit list</h4>
+         <div className="List_arrow">
          <BsArrowsAngleExpand/>
+         </div>
         
         </div>
         <div className="List_two">
         <h4>Household doit list</h4>
-        <BsArrowsAngleExpand/>
-       
+        <div className="List_arrow">
+         <BsArrowsAngleExpand/>
+         </div>
         </div>
         </>  
     );
