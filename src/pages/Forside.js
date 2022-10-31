5c1e96746dc616548edaeaebebdfcea2b31b35ea
MissSkydstofte <rikkestorgaardc@gmail.com>
2022-10-31 11:31:43 +0100

    Rikke

--- src/pages/Forside.js
+++ src/pages/Forside.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NavLink } from "react-router-dom";
 import PersonIkon from '../components/PersonIkon';
 import Opgaver from '../components/Opgaver';
